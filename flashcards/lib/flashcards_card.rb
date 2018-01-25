210731b7e96942154007aadf3b8d7fee44246fcf
hmesander <haleymesander@gmail.com>
2018-01-24 20:25:30 -0700

    Initial commit of Flashcards Project - Iteration 1 - lib file

---
+++ flashcards/lib/flashcards_card.rb
@@ -0,0 +1,13 @@
+class Card
+  attr_reader :question, :answer
+
+  def initialize(question, answer)
+      @question = question
+      @answer = answer
+  end
+
+end
+
+card = Card.new("What is the capital of Alaska?", "Juneau")
+puts card.question
+puts card.answer
