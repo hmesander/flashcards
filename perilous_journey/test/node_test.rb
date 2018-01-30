42e56118dd8fddd18437152e7e7b9a7c8c513745
hmesander <haleymesander@gmail.com>
2018-01-30 08:32:59 -0700

    Initial commit of perilous journey - iteration 0.

---
+++ perilous_journey/test/node_test.rb
@@ -0,0 +1,15 @@
+require './lib/node.rb'
+require 'minitest'
+require 'minitest/autorun'
+require 'minitest/pride'
+
+class NodeTest < Minitest::Test
+
+  def test_node_class_exists_and_initial_arguments
+    node = Node.new("Burke")
+    assert_instance_of Node, node
+    assert_equal "Burke", node.surname
+    assert_nil  node.next_node
+  end
+
+end
