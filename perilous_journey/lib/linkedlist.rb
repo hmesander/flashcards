4a21eb02f6351eca2fff91e6ecf2c05127628510
hmesander <haleymesander@gmail.com>
2018-01-30 21:57:49 -0700

    Revisit of perilous journey - iteration 2 with corrections.

--- perilous_journey/lib/linkedlist.rb
+++ perilous_journey/lib/linkedlist.rb
@@ -6,29 +6,34 @@ class LinkedList
 
   def initialize(head=nil)
     @head = head
-    @data = []
+    @list_count = 0
   end
 
   def append(data)
-      if @head == nil
-        @data << data
-        @head = Node.new(data)
-      else
-        @data << data
-        next_node(data)
+    new_node = Node.new(data)
+    if @head == nil
+      @list_count += 1
+      @head = new_node
+    else
+      current_node = @head
+      until current_node.next_node == nil
+        current_node = current_node.next_node
       end
+      @list_count += 1
+      current_node.next_node = new_node
+    end
   end
 
-  def next_node(data)
-    @head.next_node = Node.new(data)
+  def next_node
+    @head.next_node
   end
 
   def count
-    @data.count
+    @list_count
   end
 
   def to_string
-   "The #{@data[0]} family, followed by the #{@data[1]} family"
+   "The #{@head.surname} family, followed by the #{@head.next_node.surname} family."
   end
 
 end
--- perilous_journey/test/linkedlist_test.rb
+++ perilous_journey/test/linkedlist_test.rb
@@ -16,36 +16,35 @@ class LinkedListTest < Minitest::Test
   def test_for_append_method
     list = LinkedList.new
     assert_nil list.head
-    new_node = list.append("West")
-    assert_equal list.head.surname, new_node.surname
-    new_node = list.append("Smith")
-    assert_equal list.next_node("Smith").surname, new_node.surname
-    new_node = list.append("Blub")
-    assert_equal list.next_node("Blub").surname, new_node.surname
-    puts list.count
+    node_just_added = list.append("West")
+    assert_equal list.head.surname, node_just_added.surname
+    node_just_added = list.append("Hardy")
+    assert_equal list.head.next_node.surname, node_just_added.surname
+    node_just_added = list.append("Mesander")
+    assert_equal list.head.next_node.next_node.surname, node_just_added.surname
   end
 
-  def test_for_head_next_node_method
+  def test_for_next_node_method
     list = LinkedList.new
     list.append("West")
     assert_nil list.head.next_node
-    new_node = list.append("Smith")
-    assert_equal new_node,list.head.next_node
   end
 
   def test_for_count_method
     list = LinkedList.new
     list.append("West")
     assert_equal 1, list.count
-    list.append("Smtih")
+    list.append("Hardy")
     assert_equal 2, list.count
+    list.append("Mesander")
+    assert_equal 3, list.count
   end
 
   def test_for_to_string_method
     list = LinkedList.new
     list.append("West")
-    list.append("Smith")
-    assert_equal "The West family, followed by the Smith family", list.to_string
+    list.append("Hardy")
+    assert_equal "The West family, followed by the Hardy family.", list.to_string
   end
 
 end
